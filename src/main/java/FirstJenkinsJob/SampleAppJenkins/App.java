1b9ce39a5320701782a474a6c4b92a28b5ef1a29
kirankumarcvv <kirankumarcvv@gmail.com>
2018-10-31 10:26:27 +0530

    Java files for Jenkins

--- src/main/java/FirstJenkinsJob/SampleAppJenkins/App.java
+++ src/main/java/FirstJenkinsJob/SampleAppJenkins/App.java
@@ -6,8 +6,32 @@ package FirstJenkinsJob.SampleAppJenkins;
  */
 public class App 
 {
+	
+	static int a=10; 
+	
+	static int b=20; 
+	
+	private static String n = "Jenkins Demo";
+	
     public static void main( String[] args )
     {
+    	int c = add(10,20);
+    	
         System.out.println( "Hello World!" );
+        
+        System.out.println("Sum of 2 numbers is : " + c);
+        
+        System.out.println("Value of a is: " + a);
+        
+        System.out.println("Value of a is " + b);
+        
+        System.out.println(n);
+        
+        
     }
+
+	public static int add(int i, int j) {
+		
+		return i+j;
+	}
 }
